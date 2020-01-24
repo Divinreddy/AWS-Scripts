d93a4d0efe2335e1fc0b10356dd9b77c617e5306
Divin Reddy <36403034+Divinreddy@users.noreply.github.com>
2020-01-24 09:31:09 +0000

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
  # Stop EC2 Single Instance
  # Prerequisite - instanceId script needs to be present before using this command 
- # © Jeetendra Choudhary - www.jeetendra.xyz
+ # © Divin Reddy - www.divin.xyz
 aws ec2 stop-instances --instance-ids `sh instanceId.sh | grep "^i"`
